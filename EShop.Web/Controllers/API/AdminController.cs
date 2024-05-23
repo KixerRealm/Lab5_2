2f365c60dd2b8e12738b4279a2a330e229c21471
Kristijan Kostovski <kristijan.kostovski@teaming.inteligenta.io>
2024-05-23 16:17:32 +0200

    Add project files.

---
+++ EShop.Web/Controllers/API/AdminController.cs
@@ -0,0 +1,63 @@
+﻿using EShop.Domain.Domain;
+using EShop.Domain.Identity;
+using EShop.Service.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Movie_App.Service.Interface;
+using Org.BouncyCastle.Utilities;
+
+namespace EShop.Web.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IOrderService _orderService;
+        private readonly IMovieService _movieService;
+        public AdminController(IOrderService orderService,IMovieService movieService)
+        {
+            _orderService = orderService;
+            this._movieService = movieService;
+        }
+
+        [HttpGet]
+        public List<Order> Index()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+
+
+        [HttpGet("[action]")]
+        public List<Order> GetAllOrders()
+        {
+            return _orderService.GetAllOrders();
+        }
+
+
+        [HttpPost("[action]")]
+        public Order GetDetailsForOrder(BaseEntity model)
+        {
+            return _orderService.GetDetailsForOrder(model);
+        }
+
+        [HttpPost("[action]")]
+        public bool ImportAllMovies(List<Movie> model)
+        {
+            bool status = true;
+
+            foreach (var item in model)
+            {
+                var movie = new Movie
+                    {
+                        MovieDescription = item.MovieDescription,
+                        MovieName = item.MovieName,
+                        MovieImage = item.MovieImage
+                    };
+                    _movieService.CreateNewMovie(movie);
+                }
+            return status;
+        }            
+    }
+}
